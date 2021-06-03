1ea3021486b3df6a5019a1766d9613c1bc47d49b
Tristan Trébaol <tristan.trebaol@epfl.ch>
2021-06-04 00:22:00 +0100

    start recovery work

---
+++ recovery/init.m
@@ -0,0 +1,6 @@
+clear;
+addpath /Users/tristan.trebaol/Documents/PDM/Project/Code/smartcare/;
+basedir = setBaseDir();
+subfolder = 'MatlabSavedVariables';
+study = 'BR';
+plotfolder = '../../../Plots/Recovery';
