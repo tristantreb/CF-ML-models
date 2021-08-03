5dd3348f6a529f241502ea78fe314e6e5840c071
Tristan Trébaol <tristan.trebaol@epfl.ch>
2021-08-03 18:15:10 +0200

    plot #measures heatmap for a set of measures "mm"

--- recovery/createHeatMapInterventions.m
+++ recovery/createHeatMapInterventions.m
@@ -6,83 +6,38 @@ init;
 load(fullfile(basedir, subfolder, 'breatheclinicaldata.mat'),'brPatient');
 cdPatient=brPatient; clear brPatient;
 
-modelinputfile = 'BRalignmentmodelinputs_recovery_gap10_datawind_20.mat';
+modelinputfile = 'BRalignmentmodelinputs_recovery_gap10_datawind20.mat';
 % load amInterventions, amDatacube, measures and count info
 load(fullfile(basedir, subfolder, modelinputfile));
 
-temp = hsv(64);
-brightness = 0.9;
-
 %% 
-if ismember(study, {'SC', 'TM'})
-    colors(1,:)  = temp(4,:);
-    colors(2,:)  = temp(6,:);
-    colors(3,:)  = temp(8,:);
-    colors(4,:)  = temp(10,:);
-    colors(5,:)  = temp(12,:);
-    colors(6,:)  = temp(14,:);
-    colors(7,:)  = temp(16,:);
-    colors(8,:)  = temp(18,:);
-    colors(9,:)  = temp(20,:);
-    colors(10,:)  = [1 0 1];
-    nmeasures = 9;
-elseif ismember(study, {'CL'})
-    colors(1,:)  = temp(4,:);
-    colors(2,:)  = temp(6,:);
-    colors(3,:)  = temp(7,:);
-    colors(4,:)  = temp(8,:);
-    colors(5,:)  = temp(9,:);
-    colors(6,:)  = temp(10,:);
-    colors(7,:)  = temp(11,:);
-    colors(8,:)  = temp(12,:);
-    colors(9,:)  = temp(13,:);
-    colors(10,:)  = temp(14,:);
-    colors(11,:)  = temp(15,:);
-    colors(12,:)  = temp(16,:);
-    colors(13,:)  = temp(17,:);
-    colors(14,:)  = temp(18,:);
-    colors(15,:)  = temp(20,:);
-    colors(16,:)  = [1 0 1];
-    nmeasures = 15;
-elseif ismember(study, {'BR'})
-    colors(1,:)  = temp(4,:);
-    colors(2,:)  = temp(6,:);
-    colors(3,:)  = temp(7,:);
-    colors(4,:)  = temp(8,:);
-    colors(5,:)  = temp(9,:);
-    colors(6,:)  = temp(10,:);
-    colors(7,:)  = temp(11,:);
-    colors(8,:)  = temp(12,:);
-    colors(9,:)  = temp(13,:);
-    colors(10,:)  = temp(14,:);
-    colors(11,:)  = temp(15,:);
-    colors(12,:)  = temp(16,:);
-    colors(13,:)  = temp(17,:);
-    colors(14,:)  = temp(18,:);
-    colors(15,:)  = temp(19,:);
-    colors(16,:)  = temp(22,:);
-    colors(17,:)  = temp(25,:);
-    colors(18,:)  = [1 0 1]; % pink for treatment end
-    colors(19,:)  = temp(32,:); % cyan for treatment start
-    nmeasures = 19;
-else
-    fprintf('**** Unknown Study ****');
-    return;
+% choose measure mask
+daysprior = 35;
+dayspost = 39;
+
+for measuresmask = [0 21 22 30 31 32 33]
+    plotHeatMap(measuresmask, daysprior, dayspost, amInterventions, physdata, measures, study);
+    % save value
+    filename = sprintf('%s-HeatmapInterventions-mm%i', study, measuresmask);
+    subfolder = sprintf('Plots/%s', study);
+    if ~exist(strcat(basedir, plotfolder), 'dir')
+        mkdir(strcat(basedir, plotfolder));
+    end
+    saveas(gcf,fullfile(plotfolder,[filename '.png']))
+    close all;
 end
 
-colors(1:end , :) = colors(1:end,:) .* brightness;
 
-% % get the date scaling offset for each patient
-% patientoffsets = getPatientOffsets(physdata);
+%% helper functions
 
-%% 
-days_prior = 35;
-days_post = 39;
+function plotHeatMap(mm, days_prior, days_post, amInterventions, physdata, measures, study)
+
 intrwindow = days_prior+days_post+1;
+ninterventions = size(amInterventions,1);
+
 intrrangetable = table('Size',[ninterventions*intrwindow 6],...
     'VariableTypes',{'double','double','double','string','string','double'},...
     'VariableNames',{'ID','Date','IntrDate','Label','Route','Index'});
-% nan(ninterventions*intrwindow,4);
 
 for i = 1:ninterventions
     start = amInterventions.IVDateNum(i);
@@ -101,11 +56,19 @@ idxlabel = strsplit(sprintf(" - Intr %i - ,",intrrangetable.Index),',')';
 idlabel = strsplit(sprintf("ID %i,",intrrangetable.ID),',')';
 intrrangetable.Label = intrrangetable.Route + idxlabel(1:end-1) + idlabel(1:end-1);
 
-% create a table of counts of measures by patient/day (@max function here
-% is irrelevant as we just want the group counts
-pdcountmtable = varfun(@max, physdata(:, {'SmartCareID','DateNum'}), 'GroupingVariables', {'SmartCareID', 'DateNum'});
+% fitler corresponding recordings
+measures = getMeasuresMask(mm, measures);
+physdatafiltered = physdata(ismember(physdata.RecordingType,measures.Name(logical(measures.Mask))),:);
 
+% create a table of counts of measures by patient/day (@max function here
+% is irrelevant as we just want the group counts)
+pdcountmtable = varfun(@max, physdatafiltered(:, {'SmartCareID','DateNum'}), 'GroupingVariables', {'SmartCareID', 'DateNum'});
+% add groupcount to intervention table
 intrrangetable = outerjoin(intrrangetable,pdcountmtable,'Type', 'Left','Leftkeys',{'ID' 'Date'},'RightKeys',{'SmartCareID' 'DateNum'});
+
+% define colors
+nmeasures = max(intrrangetable.GroupCount);
+colors = getColors(nmeasures);
 
 % if stop date belongs to data range, change color
 % ncqnnot vectorize as many dates can be spotted for one intervention
@@ -114,11 +77,11 @@ for i = 1:size(intrrangetable,1)
     if sum(ismember(amInterventions.SmartCareID,intrrangetable.ID(i)) ...
             & ismember(amInterventions.IVStopDateNum,intrrangetable.Date(i)))
         
-        intrrangetable.GroupCount(i) = 18;
+        intrrangetable.GroupCount(i) = nmeasures+1;
     elseif sum(ismember(amInterventions.SmartCareID,intrrangetable.ID(i)) ...
             & ismember(amInterventions.IVDateNum,intrrangetable.Date(i)))
         
-        intrrangetable.GroupCount(i) = 19;
+        intrrangetable.GroupCount(i) = nmeasures+2;
     end
 end
 
@@ -129,21 +92,30 @@ f = figure('Name', title,'Position', [1 1 2000 1000]);
 p = uipanel('Parent',f,'BorderType','none', 'BackgroundColor', 'white');
 h = heatmap(p, intrrangetable, 'IntrDate', 'Label', 'Colormap', colors, 'MissingDataColor', 'black', ...
     'ColorVariable','GroupCount','ColorMethod','max', 'MissingDataLabel', 'No data');
-h.Title = sprintf('Heatmap of measures for the %i interventions. Legend: cyan = start date, pink = stop date',ninterventions);
+h.Title = [sprintf('Heatmap of #measures for the %i interventions, mm = %i\n Measures:  %s\n',ninterventions,mm, ...
+    string(join(measures.DisplayName(logical(measures.Mask)),', '))),...
+    'Legend: {\color[rgb]{0.9 0 0.9}Interventions start}, {\color[rgb]{0 0.9 0.8156}Interventions end}'];
+
 h.XLabel = 'Days';
 h.YLabel = 'Intervention';
 h.CellLabelColor = 'none';
 h.GridVisible = 'off';
-h.ColorLimits = [0 19];
-
-filename = sprintf('%s-HeatmapInterventions', study);
-subfolder = sprintf('Plots/%s', study);
-if ~exist(strcat(basedir, plotfolder), 'dir')
-    mkdir(strcat(basedir, plotfolder));
-end
+h.ColorLimits = [1 size(colors,1)+1];
 set(gca,'FontSize',6);
-saveas(gcf,fullfile(plotfolder,[filename '.png']))
-close(f);
+end
 
-%% plot only the measures used in model
+function colors = getColors(nmeasures)
+% create color scale for the heatmap
+temp = hsv(64);
+brightness = 0.9;
 
+colors = zeros(nmeasures+2, 3);
+
+idx = ceil(linspace(4,23,nmeasures));
+
+colors(1:nmeasures,:) = temp(idx,:);
+colors(end,:)  = [1 0 1]; % pink for treatment end
+colors(end-1,:)  = temp(32,:); % cyan for treatment start
+
+colors = colors .* brightness;
+end
